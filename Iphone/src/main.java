d82661f4d3d9882a94296477d8b58efd82d2b87a
Gabriel Garcia <gabrielgarcia.ata@gmail.com>
2024-06-17 16:59:35 -0300

    Finalização da implementação dos métodos

--- Iphone/src/main.java
+++ Iphone/src/main.java
@@ -1,3 +1,29 @@
-public class main {
-    
+public class main  {
+    public static void main(String[] args) {
+        Iphone iphone = new Iphone("Iphone 14 Pro Max","1420242301");
+
+    //Reprodutor Musical
+    iphone.selecionarMusica("The Last of the real Ones");
+    iphone.tocar();
+    iphone.pausar();
+
+    //Navegador de Internet
+    iphone.adicionarNovaAba();
+    iphone.exibirPagina("https://github.com/GabrielGarciaUSP/Bootcamp-Santander");
+    iphone.atualizarPagina();
+
+    //Aparelho Telefônico 
+    iphone.ligar("(18)912345678");
+    iphone.atender();
+    iphone.iniciarCorreioVoz();
+
+    //Métodos getters e setters 
+    System.out.println("Modelo do iPhone: " + iphone.getModelo());
+    iphone.setModelo("iPhone 15 Pro Max");
+    System.out.println("Modelo do iPhone: " + iphone.getModelo());
+    System.out.println("Número de série do iPhone: " + iphone.getNumeroSerie());
+    iphone.setNumeroSerie("1520242301");
+    System.out.println("Novo número de série do iPhone: " + iphone.getNumeroSerie());
+
+    }
 }
